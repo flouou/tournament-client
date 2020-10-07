e829ce6a93aa233adf05ced2f37b75ad4dfed07d
Florian Bindschädel <florian.bindschaedel@bfits.com>
2020-10-07 07:04:48 +0200

    Added filter to schedule view and rating formatter

--- src/views/Schedule.vue
+++ src/views/Schedule.vue
@@ -1,5 +1,20 @@
+/* eslint-disable vue/no-use-v-if-with-v-for */
 <template>
   <div class="schedule">
+    <div class="text-right">
+      <button type="button"
+              class="btn btn-primary ml-2 mb-2"
+              @click="filterTable('')">
+                All
+      </button>
+      <button type="button"
+              class="btn btn-primary ml-2 mb-2"
+              v-for="classification in classifications"
+              :key="classification.id"
+              @click="filterTable(classification.id)">
+                {{classification.name}}
+      </button>
+    </div>
     <table class="table table-sm">
       <thead>
         <tr>
@@ -7,6 +22,8 @@
           <th scope="col">Group</th>
           <th scope="col">Club</th>
           <th scope="col">Category</th>
+          <th scope="col">Rating</th>
+          <th scope="col"></th>
         </tr>
       </thead>
       <tbody>
@@ -15,6 +32,12 @@
           <td>{{group.name}}</td>
           <td>{{group.club.name}}</td>
           <td>{{group.classification.name}}</td>
+          <td>{{ formatRating(group.showRating) }}</td>
+          <td><button type="button"
+              class="btn btn-success ml-2 mb-2"
+              @click="editEntry(group.id)">
+                Edit
+      </button></td>
         </tr>
       </tbody>
     </table>
@@ -28,6 +51,7 @@ export default {
   data() {
     return {
       groups: [],
+      filter: '',
     };
   },
   async mounted() {
@@ -36,7 +60,36 @@ export default {
   },
   computed: {
     sortedGroups() {
-      return [...this.groups].sort((a, b) => a.time > b.time);
+      let sortedGroups = [...this.groups].sort((a, b) => a.time > b.time);
+      sortedGroups = sortedGroups.filter((group) => group.classification.id === this.filter || this.filter === '');
+      return sortedGroups;
+    },
+    classifications() {
+      let classes = [];
+      this.groups.forEach((group) => {
+        if (group.classification != null
+            && group.classification !== undefined
+            && group.classification.id !== undefined) {
+          classes[group.classification.id] = group.classification;
+        }
+      });
+      classes = classes.filter((classification) => classification !== undefined);
+      return classes;
+    },
+  },
+  methods: {
+    filterTable(classId) {
+      this.filter = classId;
+    },
+    formatRating(showRating) {
+      if (showRating !== null) {
+        let sum = 0;
+        showRating.ratings.forEach((rating) => {
+          sum += rating.value;
+        });
+        return sum;
+      }
+      return '';
     },
   },
 };
